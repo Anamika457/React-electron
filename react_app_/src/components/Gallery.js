27470c7b96f60c1f2dd26343590d2888e19ba2c9
Anamika457 <143307770+Anamika457@users.noreply.github.com>
2024-01-06 18:06:28 +0530

    Update Gallery.js

--- react_app_/src/components/Gallery.js
+++ react_app_/src/components/Gallery.js
@@ -1,1 +1,24 @@
 
+
+import React from 'react';
+
+const Gallery = ({ images, handleImageClick }) => {
+  return (
+    <div className='gallery-container'>
+      {images.map((image, index) => (
+  <div key={index} className='gallery-item'>
+    <img
+      src={image.edited || image.original}
+      alt={`Image ${index + 1}`}
+      onClick={() => handleImageClick(image.original, image.edited, index)}
+      className='gallery-image'
+    />
+  </div>
+))}
+
+    </div>
+  );
+};
+
+export default Gallery;
+
